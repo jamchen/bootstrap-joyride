d0933687163a60a9264ebac7f4d61ac7064f7598
James Chen <jameschen@actions-micro.com>
2015-04-10 12:46:13 +0800

    Add title icon support

--- bootstrap-joyride.js
+++ bootstrap-joyride.js
@@ -108,10 +108,14 @@
               return;
             }
           }
+          var titleIcon = "";
+          if (tip_data['titleiconclass']) {
+            titleIcon = "<span class=\""+tip_data['titleiconclass']+"\"></span>&nbsp;";
+          }
           $target.popover({
             html : true,
             trigger: 'manual|focus',
-            title: tip_data['title'] ? "" + tip_data['title'] + "  <a class=\"tour-tip-close close\" data-touridx=\"" + (idx + 1) + "\">&times;</a>" : null,
+            title: tip_data['title'] ? titleIcon + tip_data['title'] + "  <a class=\"tour-tip-close close\" data-touridx=\"" + (idx + 1) + "\">&times;</a>" : null,
             content: "<p>" + ($li.html()) + "</p><p style=\"text-align: right\"><a href=\"#\" class=\"tour-tip-next btn\" data-touridx=\"" + (idx + 1) + "\">" + ((idx + 1) < $tips.length ? 'Next <i class="icon-chevron-right"></i>' : '<i class="icon-ok"></i> Done') + "</a></p>",
             placement: tip_data['placement'] || 'right',
             container: tip_data['container'] ? $(tip_data['container']) : false,
